8279c925a784860e77942362beaa6150bddcfb81
dime <dime@localhost.localdomain>
2017-05-27 04:47:29 -0300

    sai da frente satanas

---
+++ PHASER/js/enemies.js
@@ -0,0 +1,218 @@
+var inheritsFrom = function (child, parent) {
+    child.prototype = Object.create(parent.prototype);
+};
+
+var enemies = [];
+
+Torreta = function (index, game, player, bullets, x, y) {
+
+    this.game = game;
+    this.health = 3;
+    this.player = player;
+    this.bullets = bullets;
+    this.fireRate = 1000;
+    this.nextFire = 0;
+    this.alive = true;
+
+    this.baseTorreta = game.add.sprite(x, y, 'torretbase');
+    this.torreta = game.add.sprite(x, y, 'torret');
+    this.baseTorreta.anchor.set(0.5);
+    this.torreta.scale.setTo(1.2, 1.2);
+    this.torreta.anchor.set(0.3, 0.4);
+
+    this.baseTorreta.name = index.toString();
+    game.physics.enable(this.baseTorreta, Phaser.Physics.ARCADE);
+    this.baseTorreta.body.immovable = true;
+    this.baseTorreta.body.collideWorldBounds = true;
+
+    this.torreta.angle = game.rnd.angle();
+}
+
+Tornera = function (index, game, player, bullets, x, y) {
+
+    this.game = game;
+    this.health = 3;
+    this.player = player;
+    this.bullets = bullets;
+    this.fireRate = 200;
+    this.nextFire = 0;
+    this.alive = true;
+
+    this.baseTorreta = game.add.sprite(x, y, 'torretbase');
+    this.torreta = game.add.sprite(x, y, 'torneira', 0);
+    this.torreta.anchor.set(0.5);
+    this.baseTorreta.anchor.set(0.5);
+    this.torreta.scale.setTo(1.2, 1.2);
+
+    this.baseTorreta.name = index.toString();
+    game.physics.enable(this.baseTorreta, Phaser.Physics.ARCADE);
+    this.baseTorreta.body.immovable = true;
+    this.baseTorreta.body.collideWorldBounds = true;
+
+    this.torreta.angle = game.rnd.angle();
+}
+
+Zeruela = function (index, game, player, bullets, x, y) {
+
+    this.game = game;
+    this.health = 3;
+    this.player = player;
+    this.bullets = bullets;
+    this.fireRate = 200;
+    this.nextFire = 0;
+    this.alive = true;
+
+    this.torreta = game.add.sprite(x, y, 'zeruela', 0);
+    this.torreta.anchor.set(0.5);
+    this.torreta.scale.setTo(1.2, 1.2);
+
+    this.torreta.name = index.toString();
+    game.physics.enable(this.torreta, Phaser.Physics.ARCADE);
+    this.torreta.body.immovable = true;
+    this.torreta.body.collideWorldBounds = true;
+
+    this.torreta.angle = game.rnd.angle();
+}
+
+Avoa = function (index, game, player, bullets, x, y) {
+    // body...
+}
+
+Enemy = function () {}
+
+Enemy.prototype.damage = function() {
+
+    this.health -= 1;
+
+    if (this.health <= 0)
+    {
+        this.alive = false;
+
+        // this.baseTorreta.kill();
+        this.torreta.kill();
+        score++;
+
+        return true;
+    }
+    return false;
+
+}
+
+Enemy.prototype.update = function() {
+
+    this.baseTorreta.x = this.torreta.x
+    this.baseTorreta.y = this.torreta.y
+
+    this.torreta.rotation = this.game.physics.arcade.angleBetween(this.torreta, this.player);
+
+    if (this.game.physics.arcade.distanceBetween(this.torreta, this.player) < 500)
+    {
+        if (this.game.time.now > this.nextFire && this.bullets.countDead() > 0)
+        {
+            this.nextFire = this.game.time.now + this.fireRate;
+
+            var bullet = this.bullets.getFirstDead();
+
+            bullet.reset(this.torreta.x, this.torreta.y);
+
+            bullet.rotation = this.game.physics.arcade.moveToObject(bullet, this.player, 500);
+        }
+    }
+
+}
+
+inheritsFrom(Torreta, Enemy);
+inheritsFrom(Tornera, Enemy);
+
+Tornera.prototype.update = function() {
+
+    this.baseTorreta.x = this.torreta.x
+    this.baseTorreta.y = this.torreta.y
+
+    this.torreta.rotation += 0.1;
+
+    if (this.game.physics.arcade.distanceBetween(this.torreta, this.player) < 500)
+    {
+        if (this.game.time.now > this.nextFire && this.bullets.countDead() > 0)
+        {
+            this.nextFire = this.game.time.now + this.fireRate;
+
+            var bullet = this.bullets.getFirstDead();
+
+            bullet.reset(this.torreta.x, this.torreta.y);
+
+            bullet.rotation = this.torreta.rotation;
+
+            game.physics.arcade.velocityFromRotation(bullet.rotation, 100, bullet.body.velocity);
+        }
+    }
+}
+
+function createEnemies () {
+
+    //  The enemies bullet group
+    enemyBullets = game.add.group();
+    enemyBullets.enableBody = true;
+    enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
+    enemyBullets.createMultiple(100, 'bullet');
+
+    enemyBullets.setAll('anchor.x', 0.5);
+    enemyBullets.setAll('anchor.y', 0.5);
+    enemyBullets.setAll('outOfBoundsKill', true);
+    enemyBullets.setAll('checkWorldBounds', true);
+
+    //  The torneras bullet group
+    tornerasBullets = game.add.group();
+    tornerasBullets.enableBody = true;
+    tornerasBullets.physicsBodyType = Phaser.Physics.ARCADE;
+    tornerasBullets.createMultiple(100, 'laser');
+
+    tornerasBullets.setAll('anchor.x', 0.5);
+    tornerasBullets.setAll('anchor.y', 0.5);
+    tornerasBullets.setAll('outOfBoundsKill', true);
+    tornerasBullets.setAll('checkWorldBounds', true);
+
+    enemies.push(new Torreta(0, game, tank, enemyBullets, -60, 200));
+    enemies.push(new Torreta(1, game, tank, enemyBullets, -200, -100));
+    // enemies.push(new Torreta(2, game, tank, enemyBullets, 0, 0));
+    // enemies.push(new Torreta(3, game, tank, enemyBullets, 100, 100));
+    enemies.push(new Tornera(2, game, tank, tornerasBullets, -100, 100));
+}
+
+function updateEnemies () {
+
+  // Torretas
+  game.physics.arcade.overlap(enemyBullets, tank, bulletHitPlayer, null, this);
+  game.physics.arcade.overlap(tornerasBullets, tank, bulletHitPlayer, null, this);
+
+  for (var i = 0; i < enemies.length; i++) {
+    if (enemies[i].alive) {
+      game.physics.arcade.collide(tank, (enemies[i].baseTorreta || enemies[i].torreta));
+      game.physics.arcade.overlap(bullets, (enemies[i].baseTorreta || enemies[i].torreta), bulletHitEnemy, null, this);
+      enemies[i].update();
+    }
+  }
+
+
+}
+
+function bulletHitPlayer (player, bullet) {
+
+    bullet.kill();
+    life--;
+}
+
+function bulletHitEnemy (enemy, bullet) {
+
+    bullet.kill();
+
+    var destroyed = enemies[enemy.name].damage();
+
+    if (destroyed)
+    {
+        var explosionAnimation = explosions.getFirstExists(false);
+        explosionAnimation.reset(enemy.x, enemy.y);
+        explosionAnimation.play('kaboom', 30, false, true);
+    }
+
+}
--- PHASER/js/initialize.js
+++ PHASER/js/initialize.js
@@ -4,20 +4,27 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 
 function preload () {
 
+    game.load.audio('funkaoneuvosor', 'assets/funkao.mp3');
     game.load.image('logo', 'assets/logo.png');
     game.load.image('bullet', 'assets/bullet.png');
-    game.load.image('room', 'assets/room.jpg');
-    game.load.image('fundo', 'assets/fundo.jpg');
+    game.load.image('laser', 'assets/laser.png');
+    game.load.image('fundo', 'assets/bacg.png');
     game.load.image('life', 'assets/life.png');
+    game.load.image('firstaid', 'assets/firstaid.png');
+    game.load.image('tati', 'assets/tatizaqui.png');
     game.load.spritesheet('star', 'assets/star.png', 13, 22, 4);
+    game.load.spritesheet('zeruela', 'assets/tanque.png', 70, 52, 2);
     game.load.spritesheet('kaboom', 'assets/explosion.png', 100, 100, 6);
     game.load.spritesheet('energybar', 'assets/energy.png', 150, 30, 3);
     game.load.spritesheet('torret', 'assets/torret.png', 39, 16, 2);
     game.load.spritesheet('torretbase', 'assets/torretbase.png', 36, 46, 6);
     game.load.spritesheet('corpo', 'assets/CORPO.png', 60, 36, 3);
     game.load.spritesheet('pernas', 'assets/PERNAS.png', 60, 36, 5);
+    game.load.spritesheet('torneira', 'assets/torneiras.png', 64, 64, 2);
+    game.load.spritesheet('pickups', 'assets/pickups.png', 34, 25, 8);
 
 }
+var funkao;
 
 var land;
 
@@ -33,12 +40,14 @@ var enemiesTotal = 0;
 var enemiesAlive = 0;
 var explosions;
 
+var pickupok = [];
 var logo;
 var stars;
 var score = 0;
 var energy = 0;
 var life = 20;
 var torreta;
+var tati;
 
 var move = 0;
 
@@ -51,6 +60,7 @@ var nextFire = 0;
 
 function create () {
 
+  funkao = game.add.audio('funkaoneuvosor');
   upKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
   downKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
   leftKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
@@ -58,18 +68,23 @@ function create () {
   space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
   explo = game.input.keyboard.addKey(Phaser.Keyboard.E);
 
+pickupok[0] = 0;
+pickupok[1] = 0;
+pickupok[2] = 0;
+pickupok[3] = 0;
 
+sounds = [ funkao ];
+
+game.sound.setDecodedCallback(sounds, start, this);
 
     //  Resize our game world to be a 2000 x 2000 square
     game.world.setBounds(-1000, -1000, 2000, 2000);
 
 
-    //ROOM!!!!
-    room = game.add.sprite(200,200, 'room')
     fundo = game.add.sprite(-1000, -1000, 'fundo');
 
     //  The base of our tank
-    tank = game.add.sprite(50, 27, 'pernas', 1);
+    tank = game.add.sprite(40, 30, 'pernas', 1);
     tank.anchor.setTo(0.40, 0.43);
 
 
@@ -118,13 +133,12 @@ function create () {
     tank.bringToTop();
     turret.bringToTop();
 
-    //Torretas
-    torreta1 = new Torreta(1, game, tank, enemyBullets, 100, 100);
 
     logo = game.add.sprite(0, 0, 'logo');
     logo.fixedToCamera = true;
+    game.time.events.add(Phaser.Timer.SECOND * 1, fadePicture, this);
 
-    game.input.onDown.add(removeLogo, this);
+    //game.input.onDown.add(removeLogo, this);
 
     game.camera.follow(tank);
     game.camera.deadzone = new Phaser.Rectangle(150, 150, 500, 300);
@@ -138,24 +152,59 @@ function create () {
     //  We will enable physics for any star that is created in this group
     stars.enableBody = true;
 
+    pickups = game.add.group();
+    pickups.enableBody = true;
 
 
+    if (pickupok[0] == 1){
+      var pickup = pickups.create(-120, 150, 'pickups',0);
+    } else {
+      var pickup = pickups.create(-120, 150, 'pickups',4);
+    }
+      pickup.body.immovable = true;
 
+  if (pickupok[1] == 1){
+    var pickup = pickups.create(175, 155, 'pickups',1);
+  } else {
+    var pickup = pickups.create(175, 155, 'pickups',5);
+  }
+    pickup.body.immovable = true;
 
+  if (pickupok[2] == 1){
+    var pickup = pickups.create(175, -131, 'pickups',2);
+  } else {
+    var pickup = pickups.create(175, -131, 'pickups',6);
+  }
+    pickup.body.immovable = true;
+
+  if (pickupok[3] == 1){
+    var pickup = pickups.create(-120, -128, 'pickups',3);
+  } else {
+    var pickup = pickups.create(-120, -128, 'pickups',7);
+  }
+    pickup.body.immovable = true;
+
+firstaids = game.add.group();
+firstaids.enableBody = true;
+
+for (var i = 0; i < 12; i++)
+{
+  var firstaid = firstaids.create(i * 70, 400, 'firstaid');
+
+}
     //  Here we'll create 12 of them evenly spaced apart
     for (var i = 0; i < 12; i++)
     {
         //  Create a star inside of the 'stars' group
         var star = stars.create(i * 70, 200, 'star');
         star.animations.add('walk');
-
         star.animations.play('walk', 5, true);
-
-
     }
 
     can = star.animations.add('can', [0,1,2,3], 10, true);
     star.play('can');
+
+
 
 
 //HUD
@@ -163,22 +212,28 @@ function create () {
     energybar = game.add.sprite(550, 540, 'energybar');
     energybar.fixedToCamera = true;
 
-    for (var i = 0; i <= life; i++){
-      lifebar = game.add.sprite(30+i*30,40,'life');
-      lifebar.scale.setTo(0.3,0.3);
-      lifebar.fixedToCamera = true;
-    }
+    lifebars = game.add.group();
 
-
+    tati = game.add.sprite(10,20,'tati');
+    tati.fixedToCamera = true;
 
 }
 
-function removeLogo () {
+function start() {
 
-    game.input.onDown.remove(removeLogo, this);
-    logo.kill();
+    sounds.shift();
+
+    funkao.loopFull(0.6);
 
 }
+
+function fadePicture() {
+
+    game.add.tween(logo).to( { alpha: 0 }, 2000, Phaser.Easing.Linear.None, true);
+
+}
+
+
 
 function update () {
 
@@ -186,19 +241,21 @@ function update () {
 
   //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
   game.physics.arcade.overlap(tank, stars, collectStar, null, this);
+  game.physics.arcade.overlap(tank, firstaids, collectLife, null, this);
+  game.physics.arcade.collide(tank,pickups);
 
     if (leftKey.isDown)
     {
         tank.angle = 180;
-        if(currentSpeed <= 200){
-          currentSpeed = 200;
+        if(currentSpeed <= 300){
+          currentSpeed = 300;
         }
     }
     if (rightKey.isDown)
     {
       tank.angle = 0;
-      if(currentSpeed <= 200){
-        currentSpeed = 200;
+      if(currentSpeed <= 300){
+        currentSpeed = 300;
       }
     }
 
@@ -211,8 +268,8 @@ function update () {
       } else {
         tank.angle = -90;
       }
-      if(currentSpeed <= 200){
-        currentSpeed = 200;
+      if(currentSpeed <= 300){
+        currentSpeed = 300;
       }
     } else if (downKey.isDown){
       if (leftKey.isDown){
@@ -223,8 +280,8 @@ function update () {
         tank.angle = 90;
 
       }
-      if(currentSpeed <= 200){
-        currentSpeed = 200;
+      if(currentSpeed <= 300){
+        currentSpeed = 300;
       }
     }
 
@@ -274,7 +331,7 @@ function update () {
     }
 
     if (currentSpeed > 500){
-      currentSpeed -= 100;
+      currentSpeed -= 50;
     } else if (currentSpeed > 0){
       currentSpeed -= 20;
     }
@@ -298,6 +355,14 @@ if (move == 1){
     {
         //  Boom!
         fire();
+    }
+
+    lifebars.callAll('kill');
+
+    for (var i = 1; i <= life; i++){
+      lifebar = lifebars.create(60+i*30,40,'life');
+      lifebar.scale.setTo(0.3,0.3);
+      lifebar.fixedToCamera = true;
     }
 
 }
@@ -329,7 +394,7 @@ if (arma == 0){
 function render () {
 
     // game.debug.text('Active Bullets: ' + bullets.countLiving() + ' / ' + bullets.length, 32, 32);
-    game.debug.text( 'Life:' + life + ' / Score: ' + score + ' / Speed: ' + currentSpeed, 32, 32);
+  //  game.debug.text( 'Life:' + life + ' / Score: ' + score + ' / Speed: ' + currentSpeed + ' / X: ' + tank.x + 'Y: ' + tank.y, 32, 32);
 
 }
 
@@ -348,3 +413,17 @@ function collectStar (tank, star) {
 
 }
 
+function collectLife (tank, firstaid) {
+
+    // Removes the star from the screen
+    firstaid.kill();
+
+    //  Add and update the score
+    //score += 10;
+    if (life < 20){
+      life++;
+    }
+
+
+
+}
